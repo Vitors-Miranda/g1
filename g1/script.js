96ef18bead71ee807236313221c88582ad7d0f37
Vitor Samuel Miranda de Souza <82851236+Vitors-Miranda@users.noreply.github.com>
2021-08-24 14:32:23 -0300

    Upload
    projeto inicializado há um tempo, neste commit há o upload completo do mesmo.

---
+++ g1/script.js
@@ -0,0 +1,7 @@
+var barra=window.document.getElementById('tresbarras')
+function clicar(){
+     barra.style="width: 240px;visibility: visible;"
+}
+function x(){
+        barra.style="visibility:hidden;width:0px;"
+}
